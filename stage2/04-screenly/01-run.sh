e590b890fb6de4c5ebf89f335859a46afba42ec7
Peter Ineichen <ineichen@triple-m.ch>
2019-03-21 11:24:56 +0100

    Screenly hinzugefügt

---
+++ stage2/04-screenly/01-run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+  curl -s https://bootstrap.pypa.io/get-pip.py | python3.5
+
+  git clone https://github.com/trash-box/screenly-ose.git /home/pi/screenly
+  cd /home/pi/screenly
+  git checkout master
+
+  pip install -r requirements.txt
+  mkdir -p /etc/ansible
+  echo -e "[local]\nlocalhost ansible_connection=local" | tee /etc/ansible/hosts > /dev/null
+
+  cd ansible
+  HOME=/home/pi MANAGE_NETWORK=true ansible-playbook site.yml --skip-tags enable-ssl,disable-nginx,touches_boot_partition
+  touch /home/pi/.screenly/initialized
+  chown -R pi:pi /home/pi
+
+  apt-get autoclean
+  apt-get clean
+
+  find /usr/share/doc -depth -type f ! -name copyright -delete
+  find /usr/share/doc -empty -delete
+  rm -rf /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian /usr/share/linda /var/cache/man
+  find /usr/share/locale -type f ! -name 'en' ! -name 'de*' ! -name 'es*' ! -name 'ja*' ! -name 'fr*' ! -name 'zh*' -delete
+  find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en*' ! -name 'de*' ! -name 'es*' ! -name 'ja*' ! -name 'fr*' ! -name 'zh*' -exec rm -r {} \;
+
+  HOME=/home/pi python3.5 /home/pi/screenly/dps-http.py &
+  sleep 5
+  /home/pi/screenly/bin/prepare_device_for_imaging.sh
+  pkill -f dps-http.py
+  sleep 5
+  chown -R pi:pi /home/pi
+EOF
