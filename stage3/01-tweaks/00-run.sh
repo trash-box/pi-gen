a19cef56b0d86626604218429908822fffcbd2b8
Peter Ineichen <ineichen@triple-m.ch>
2019-08-30 10:12:39 +0200

    stage3 für UFW bereinigt

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -e
 
-rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+install -v -d					"${ROOTFS_DIR}/etc/ufw"
+install -v -m 644 files/ufw.conf		"${ROOTFS_DIR}/etc/ufw/"
+
+install -v -m 640 files/user.rules		"${ROOTFS_DIR}/etc/ufw/"
+install -v -m 640 files/user6.rules		"${ROOTFS_DIR}/etc/ufw/"
+
+# NOTE: install basic rules and enable iptables
+echo "Configure firewall"
+install -m 755 files/ufw_default_deny_out       ${ROOTFS_DIR}/etc/init.d/
+install -m 755 files/deny_default_out.sh    ${ROOTFS_DIR}/usr/bin/
+chmod +x ${ROOTFS_DIR}/etc/init.d/ufw_default_deny_out
+chmod +x ${ROOTFS_DIR}/usr/bin/deny_default_out.sh
+
+on_chroot << EOF
+  systemctl enable ufw_default_deny_out
+EOF
+
---
+++ stage3/01-tweaks/files/deny_default_out.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ufw default deny outgoing
